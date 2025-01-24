ab78955134ea82e5d72d0a1950084c4ac06c4def
Yumbang <59196217+Yumbang@users.noreply.github.com>
2025-01-24 15:00:29 +0900

    Implemented static feed flow mechanism. Implemented logging callback.

--- examples/test_ode.jl
+++ examples/test_ode.jl
@@ -14,8 +14,8 @@ membrane = pristine_membrane(membrane_spec...);
 pristine_vessel = PressureVessel(7, membrane);
 
 """
-Pipe full of fluid, with `length` and cross-section `area`.
-A pipe is considered to be completely-stirred as concentration `C`.
+Pipe full of fluid, with `volume` m³.
+A pipe is considered to be completely-stirred.
 
 This struct—designed as brine circulation line—is necessary to properly model brine circulation.
 """
@@ -57,8 +57,21 @@ end
 struct SBROLoggerElement
     brine::Water
     permeate::Water
+    power_feed::typeof(1.0u"W")
+    power_circ::typeof(1.0u"W")
     time::Float64
 end
+
+mutable struct SBROParameters
+    feed_concentration::Float64
+    feed_temperature::Float64
+    feed_pressure::Float64
+    flowrate_setpoint::Float64
+    pressure_setpoint::Float64
+    process::SemiBatchRO
+    logger::SBROLoggerElement
+end
+
 """
 ODE statement function of semi-batch RO.
 Final feed means feed entering membrane vessel, after merged with circulated brine.
@@ -69,13 +82,16 @@ u[2]: Pipe (circulated brine) flow rate
 u[3]: Pipe (circulated brine) pressure
 """
 function SBRO!(du, u, p, t)
-    unpressurized_feed  = p.unpressurized_feed
+    feed_concentration  = p.feed_concentration
+    feed_temperature    = p.feed_temperature
+    feed_pressure       = p.feed_pressure
+    flowrate_setpoint   = p.flowrate_setpoint
     pressure_setpoint   = p.pressure_setpoint
     sbro                = p.process
-    logger              = p.logger
 
     # Unpressurized brine and feed
-    unpressurized_brine = Water(u[2], unpressurized_feed.T, u[1], u[3])
+    unpressurized_brine = Water(u[2], feed_temperature, u[1], u[3])
+    unpressurized_feed  = Water(max(flowrate_setpoint-u[2], 0), feed_temperature, feed_concentration, feed_pressure)
 
     # Pressurize feed and brine
     pressurized_feed,  power_feed = pump(unpressurized_feed, pressure_setpoint - unpressurized_feed.P; efficiency=0.8)
@@ -83,7 +99,7 @@ function SBRO!(du, u, p, t)
 
     # Final feed after mixing
     final_feed = junction(pressurized_brine, pressurized_feed; junction_loss=0.0)
-    # println("Feed:  $(final_feed)")
+
     # RO modeling
     brines_array, permeates_array, ΔR_ms_array = vessel_filtration(
         final_feed, sbro.pressure_vessel; dt=nothing
@@ -92,32 +108,66 @@ function SBRO!(du, u, p, t)
     final_brine    = brines_array[end][end]
     final_permeate = mix(reduce(vcat, permeates_array); pressure=1e-5)
 
-    # present_log = SBROLoggerElement(final_brine, final_permeate, t)
-    # push!(logger, present_log)
+    p.logger = SBROLoggerElement(final_brine, final_permeate, power_feed, power_circ, t)
     
     # ODE terms
+    # du[1] = (final_brine.C * final_brine.Q - u[1] * final_brine.Q) / sbro.pipe.volume
     du[1] = (final_brine.C * final_brine.Q - u[1] * final_brine.Q) / sbro.pipe.volume
     du[2] = final_brine.Q - u[2]
     du[3] = final_brine.P - u[3]
 
-    # u[2]  = final_brine.Q
-    # u[3]  = final_brine.P
-    # du[2] = 0.0
-    # du[3] = 0.0
-
     return nothing
 end
 
-logger = []
 pipe = Pipe(1.0)
 sbro = SemiBatchRO(0.8, 0.8, pipe, deepcopy(pristine_vessel))
 feed = Water(100/84, 20.0, 300/1000, 1e5);
-# u₀ = [feed.C, feed.Q, feed.P]
+logger = SBROLoggerElement(feed, feed, 0.0u"W", 0.0u"W", 0.0)
+logger_array = SBROLoggerElement[]
+
+function save_func(u, t, integrator)
+    return deepcopy(integrator.p.logger)
+end
+
+saved_values = SavedValues(typeof(0.0), typeof(logger))
+cb = SavingCallback(save_func, saved_values)
+
+# u₀ = [feed.C, 500/84, 10e5]
 u₀ = [feed.C, 0.0, 0.0]
-params = (unpressurized_feed=feed, pressure_setpoint=4e5, process=sbro, logger=logger)
+params = SBROParameters(
+    300/1000, 20.0, 1e-5,
+    1000/84, 10e5, sbro, logger
+)
 prob = ODEProblem(SBRO!, u₀, (0.0, 60.0), params)
 
-@benchmark sol = solve(prob, Tsit5())
+@benchmark sol = solve(prob, Tsit5(); callback=cb)
+
+saved_values.t
+brine_profile = profile_water(getfield.(saved_values.saveval, :brine))
+permeate_profile = profile_water(getfield.(saved_values.saveval, :permeate))
+power_profile = DataFrame(
+    :time=>getfield.(saved_values.saveval, :time)[2:end],
+    :power_feed=>getfield.(saved_values.saveval, :power_feed)[2:end],
+    :power_circ=>getfield.(saved_values.saveval, :power_circ)[2:end]
+)
+
+plot(sol.t * u"s", brine_profile.Q)
+plot!(sol.t, [u[2] for u in sol.u], title="Flowrate")
+plot!(sol.t * u"s", permeate_profile.Q)
+plot!(sol.t * u"s", [1000/84 for _ in sol.t])
+plot!(sol.t * u"s", permeate_profile.Q+brine_profile.Q)
+
+plot(power_profile.time, power_profile.power_feed)
+plot!(power_profile.time, power_profile.power_circ)
+
+plot(sol.t * u"s", brine_profile.C)
+plot!(sol.t, [u[1] for u in sol.u], title="Concentration")
+
+
+plot(sol.t * u"s", permeate_profile.C.*permeate_profile.Q)
+
+plot(sol.t * u"s", brine_profile.C.*brine_profile.Q)
+
 
 sol.u
 sol.t
@@ -132,13 +182,25 @@ plot(sol.t, [u[3] for u in sol.u], title="Pressure")
 raw_feeds           = [feed for _ in sol.t]
 # 파이프의 부피가 작으면 농축수의 농도가 거의 즉시 반영이 됨.
 plot(sol.t, [u[1]*u[2] for u in sol.u], title="Salt flow")
-plot!(sol.t, feed.C*feed.Q*sol.t)
 
 circulated_brines   = [Water(u[2], feed.T, u[1], u[3]) for u in sol.u]
-mixed_feeds         = [mix(feed, brine) for brine in circulated_brines]
-@show profile_water(circulated_brines);
-@show profile_water(mixed_feeds);
+adjusted_feeds      = [Water(1000/84-brine.Q, feed.T, 300/1000, 1e-5) for brine in circulated_brines]
+circulated_brine_profile = profile_water(circulated_brines);
+adjusted_feed_profile = profile_water(adjusted_feeds);
 
+final_feeds = [mix(row[1], row[2]) for row in eachslice(hcat(circulated_brines, adjusted_feeds), dims=1)]
+final_feed_profile = profile_water(final_feeds);
+
+plot(sol.t.*u"s", final_feed_profile.C)
+
+sixty_sec_final_feed_profile = hcat(DataFrame(:time => sol.t.*u"s"), final_feed_profile)
+
+
+plot(sixty_sec_final_feed_profile.time, sixty_sec_final_feed_profile.C .* sixty_sec_final_feed_profile.Q)
+# plot!(sol.t.*u"s", cumsum(adjusted_feed_profile.C .* adjusted_feed_profile.Q))
+plot!(sol.t.*u"s", (adjusted_feed_profile.C .* adjusted_feed_profile.Q .* sol.t))
+
+plot(sixty_sec_final_feed_profile.time, sixty_sec_final_feed_profile.Q)
 
 @show sol.t;
 @show filtered_time = [log_element for log_element in logger if log_element.time in sol.t];
