0fb8bd75d556f40a18b491566b9e577c4a41cb10
Yumbang <59196217+Yumbang@users.noreply.github.com>
2025-02-18 13:07:21 +0900

    Misc changes

--- examples/test_semibatch.jl
+++ examples/test_semibatch.jl
@@ -21,29 +21,31 @@ feed_C = 1.0u"kg/m^3"
 flowrate_setpoint = 20.0u"m^3/hr"
 pressure_setpoint = 10.0u"bar"
 
-dt      = 10.0u"s"
+dt      = 60.0u"s"
 mode    = :CC
 fouling = true
 
 next_u = nothing
 result_dfs = []
-@showprogress for cycle ∈ 1:500
+@showprogress for cycle ∈ 1:5
     global next_u
-    for i ∈ 1:480
+    for i ∈ 1:80
         result_df, next_u = process_semibatch_RO!(
             feed_T, feed_C, next_u,
             flowrate_setpoint, pressure_setpoint;
             process=sbro, dt=dt, mode=mode, fouling=fouling
         )
+        result_df.cycle .= cycle
         push!(result_dfs, result_df)
     end
-
-    for i ∈ 1:12
+    
+    for i ∈ 1:2
         result_df, next_u = process_semibatch_RO!(
             feed_T, feed_C, next_u,
             flowrate_setpoint, pressure_setpoint;
             process=sbro, dt=dt, mode=:purge, fouling=fouling
-        )
+            )
+        result_df.cycle .= cycle
         push!(result_dfs, result_df)
     end
 end
@@ -51,16 +53,18 @@ end
 total_df        = reduce(vcat, result_dfs)
 total_df.time   = cumsum(total_df.time_diff)
 
-flowrate_plot = plot(size=(750,500), legend=:right, label="Permeate Q", title="Flowrate", framestyle=:box)
-plot!(flowrate_plot, uconvert.(u"d",total_df.time), total_df.Permeate_Q, label="Permeate Q", xlabel="Time", ylabel="Flowrate", leftpad=25Plots.mm)
-plot!(flowrate_plot, uconvert.(u"d",total_df.time), total_df.Raw_brine_Q, label="Raw brine Q")
-scatter(flowrate_plot, uconvert.(u"d",total_df.time), total_df.Circulated_brine_Q, label="Circulated brine Q", markersize=2, markerstrokewidth=0.1)
+plotting_time_unit = u"d"
 
-plot(uconvert.(u"minute",total_df.time), total_df.Raw_brine_C, label="Raw brine C", framestyle=:box)
-plot!(uconvert.(u"minute",total_df.time), total_df.Circulated_brine_C, label="Circulated (disposed) brine C")
+flowrate_plot = plot(size=(750,500), legend=:right, label="Permeate Q", title="Flowrate", framestyle=:box)
+plot!(flowrate_plot, uconvert.(plotting_time_unit,total_df.time), total_df.Permeate_Q, label="Permeate Q", xlabel="Time", ylabel="Flowrate", leftpad=25Plots.mm)
+plot!(flowrate_plot, uconvert.(plotting_time_unit,total_df.time), total_df.Raw_brine_Q, label="Raw brine Q")
+scatter(flowrate_plot, uconvert.(plotting_time_unit,total_df.time), total_df.Circulated_brine_Q, label="Circulated brine Q", markersize=1, markerstrokewidth=0.1)
+
+plot(uconvert.(plotting_time_unit,total_df.time), total_df.Raw_brine_C, label="Raw brine C", framestyle=:box)
+plot!(uconvert.(plotting_time_unit,total_df.time), total_df.Circulated_brine_C, label="Circulated (disposed) brine C")
 
 pristine_vessel_profile = profile_vessel(vessel)
 fouled_vessel_profile   = profile_vessel(sbro.pressure_vessel)
 
-plot(cumsum(pristine_vessel_profile.dx), pristine_vessel_profile.R_m)
-plot!(cumsum(fouled_vessel_profile.dx), fouled_vessel_profile.R_m)
+plot(cumsum(pristine_vessel_profile.dx), pristine_vessel_profile.R_m, label="Pristine membrane")
+plot!(cumsum(fouled_vessel_profile.dx), fouled_vessel_profile.R_m, fillrange=pristine_vessel_profile.R_m, fillcolor=:black, fillstyle=:/, label="Fouled membrane")
